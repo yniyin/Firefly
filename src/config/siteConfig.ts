5d4dec632beb591a431071d1f68ee3c826cd82d8
YinHub <3127144755@qq.com>
2026-05-18 09:46:21 +0800

    Bao Yin的网站

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -131,11 +131,11 @@ export const siteConfig: SiteConfig = {
 		// 友链页面开关
 		friends: false,
 		// 赞助页面开关
-		sponsor: true,
+		sponsor: false,
 		// 留言板页面开关，需要配置评论系统
-		guestbook: true,
+		guestbook: false,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐，dev调试时只获取一页数据，build才会获取全部数据
-		bangumi: true,
+		bangumi: false,
 		// 相册页面开关
 		gallery: true,
 	},
