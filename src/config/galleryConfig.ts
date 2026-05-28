da77739346a55a2536a556febf4bf597511e7277
YinHub <3127144755@qq.com>
2026-05-28 09:43:06 +0800

    Bao Yin的网站

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -15,21 +15,13 @@ export const galleryConfig: GalleryConfig = {
 		// 每添加一个数组项就相当于添加了一个相册，记得在 public/gallery/ 目录下创建对应的子目录并放入图片
 		{
 			id: "firefly-2026",
-			name: "可爱流萤",
+			name: "壁纸",
 			description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-			location: "崩坏：星穹铁道",
+			location: "成都",
 			date: "2026-01-01",
-			tags: ["崩坏星穹铁道", "流萤"],
+			tags: ["图片", "图片1"],
 		},
 		//
-		{
-			id: "Liu",
-			name: "刘",
-			description: "mimi",
-			location: "崩坏：星穹铁道",
-			date: "2026-01-01",
-			tags: ["崩坏星穹铁道", "流萤"],
-		},
 	],
 	// 瀑布流最小列宽(px)，浏览器根据容器宽度自动计算列数，默认 240
 	// 值越小列数越多，值越大列数越少
