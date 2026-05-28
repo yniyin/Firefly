eaaba7ebc69ddbf66c374656c77793719f50e39f
YinHub <3127144755@qq.com>
2026-05-28 09:37:58 +0800

    Bao Yin的网站

--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -39,38 +39,40 @@ export const sponsorConfig: SponsorConfig = {
 			description: "使用 微信 扫码赞助",
 			enabled: true,
 		},
-		{
-			name: "ko-fi",
-			icon: "simple-icons:kofi",
-			qrCode: "",
-			link: "https://ko-fi.com/cuteleaf",
-			description: "Buy a Coffee for Firefly",
-			enabled: true,
-		},
-		{
-			name: "爱发电",
-			icon: "simple-icons:afdian",
-			qrCode: "",
-			link: "https://ifdian.net/a/cuteleaf",
-			description: "通过 爱发电 进行赞助",
-			enabled: true,
-		},
+
+		// {
+		// 	name: "ko-fi",
+		// 	icon: "simple-icons:kofi",
+		// 	qrCode: "",
+		// 	link: "https://ko-fi.com/cuteleaf",
+		// 	description: "Buy a Coffee for Firefly",
+		// 	enabled: true,
+		// },
+		// {
+		// 	name: "爱发电",
+		// 	icon: "simple-icons:afdian",
+		// 	qrCode: "",
+		// 	link: "https://ifdian.net/a/cuteleaf",
+		// 	description: "通过 爱发电 进行赞助",
+		// 	enabled: true,
+		// },
+
 	],
 
 	// 赞助者列表（可选）
 	sponsors: [
-		// 示例：已实名赞助者
-		{
-			name: "夏叶",
-			amount: "¥50",
-			date: "2025-10-01",
-		},
+		// // 示例：已实名赞助者
+		// {
+		// 	name: "夏叶",
+		// 	amount: "¥50",
+		// 	date: "2025-10-01",
+		// },
 
-		// 示例：匿名赞助者
-		{
-			name: "匿名用户",
-			amount: "¥20",
-			date: "2025-10-01",
-		},
+		// // 示例：匿名赞助者
+		// {
+		// 	name: "匿名用户",
+		// 	amount: "¥20",
+		// 	date: "2025-10-01",
+		// },
 	],
 };
